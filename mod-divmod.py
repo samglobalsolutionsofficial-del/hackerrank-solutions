d5b78d22e572b3afeceb15fb0d8021a5fedd6ce0
Sameer Khan <samglobalsolutions.official@gmail.com>
2026-07-17 13:50:45 +0500

    Solve: mod divmod

---
+++ mod-divmod.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+
+int_div = a//b
+int_mod = a%b
+divmod_patt = divmod(a, b)
+print(int_div)
+print(int_mod)
+print(divmod_patt)
