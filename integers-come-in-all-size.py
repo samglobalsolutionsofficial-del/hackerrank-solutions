b16bd7702daca6d48256fb5d27d5b249bfcae3e9
Sameer Khan <samglobalsolutions.official@gmail.com>
2026-07-17 14:20:34 +0500

    Solve: integers come in all size

---
+++ integers-come-in-all-size.py
@@ -0,0 +1,10 @@
+# Enter your code here. Read input from STDIN. Print output to STDOUT
+a = int(input())
+b = int(input())
+c = int(input())
+d = int(input())
+
+a_pow = pow(a, b)
+c_pow = pow(c, d)
+print(a_pow + c_pow)
+
